844d55d9252e58544afc990d77bae01f0e529608
trongvule39-art <trongvule39@gmail.com>
2026-06-27 21:46:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -902,7 +902,7 @@ async def slash_rscripts_by_user(interaction: discord.Interaction, username: str
 async def run_bot():
     while True:
         try:
-            await bot.start(DISCORD_TOKEN)
+            await bot.start(os.getenv("DISCORD_TOKEN"))
         except (discord.ConnectionClosed, discord.GatewayNotFound) as e:
             print(f"Disconnected due to: {e}. Reconnecting in 5 seconds...")
             await asyncio.sleep(5)
